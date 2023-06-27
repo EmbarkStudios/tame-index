c1333ad769b3c55d5d19003de12e7b478c329529
Jake Shadle <jake.shadle@embark-studios.com>
2023-06-27 09:34:09 +0200

    Add support for hashing of git urls

--- src/utils.rs
+++ src/utils.rs
@@ -27,73 +27,76 @@ pub(crate) fn encode_hex<'out, const I: usize, const O: usize>(
     }
 }
 
-/// Converts a full index url into a relative path and its canonical form
+/// The details for a remote url
+pub struct UrlDir {
+    /// The unique directory name for the url
+    pub dir_name: String,
+    /// The canonical url for the remote url
+    pub canonical: String,
+}
+
+/// Converts a url into a relative path and its canonical form
 ///
 /// Cargo uses a small algorithm to create unique directory names for any url
 /// so that they can be located in the same root without clashing
-pub fn url_to_local_dir(url: &str) -> Result<(String, String), Error> {
-    #[allow(deprecated)]
-    fn hash_u64(url: &str, registry_kind: u64) -> u64 {
-        use std::hash::{Hash, Hasher, SipHasher};
+///
+/// This function currently only supports 3 different URL kinds.
+///
+/// * (?:registry+)?<git registry url>
+/// * sparse+<sparse registry url>
+/// * git+<git repo url>
+#[allow(deprecated)]
+pub fn url_to_local_dir(url: &str) -> Result<UrlDir, Error> {
+    use std::hash::{Hash, Hasher, SipHasher};
 
-        let mut hasher = SipHasher::new_with_keys(0, 0);
-        // Registry
-        registry_kind.hash(&mut hasher);
-        // Url
-        url.hash(&mut hasher);
-        hasher.finish()
-    }
-
+    const GIT_REPO: u64 = 0;
     const GIT_REGISTRY: u64 = 2;
     const SPARSE_REGISTRY: u64 = 3;
 
-    let mut registry_kind = GIT_REGISTRY;
-
     // Ensure we have a registry or bare url
-    let (url, scheme_ind) = {
-        let scheme_ind = url.find("://").ok_or_else(|| InvalidUrl {
+    let (url, scheme_ind, kind) = {
+        let mut scheme_ind = url.find("://").ok_or_else(|| InvalidUrl {
             url: url.to_owned(),
             source: InvalidUrlError::MissingScheme,
         })?;
 
         let scheme_str = &url[..scheme_ind];
-        if scheme_str.starts_with("sparse+http") {
-            registry_kind = SPARSE_REGISTRY;
-            (url, scheme_ind)
-        } else if let Some(ind) = scheme_str.find('+') {
-            if &scheme_str[..ind] != "registry" {
+
+        let (url, kind) = match scheme_str.split_once('+') {
+            Some(("sparse", _)) => (url, SPARSE_REGISTRY),
+            // If there is no scheme modifier, assume git registry, same as cargo
+            None => (url, GIT_REGISTRY),
+            Some(("registry", _)) => {
+                scheme_ind -= 9;
+                (&url[9..], GIT_REGISTRY)
+            }
+            Some(("git", _)) => {
+                scheme_ind -= 4;
+                (&url[4..], GIT_REPO)
+            }
+            Some((_, _)) => {
                 return Err(InvalidUrl {
                     url: url.to_owned(),
                     source: InvalidUrlError::UnknownSchemeModifier,
                 }
                 .into());
             }
+        };
 
-            (&url[ind + 1..], scheme_ind - ind - 1)
-        } else {
-            (url, scheme_ind)
-        }
+        (url, scheme_ind + 3, kind)
     };
 
     // Could use the Url crate for this, but it's simple enough and we don't
     // need to deal with every possible url (I hope...)
-    let host = match url[scheme_ind + 3..].find('/') {
-        Some(end) => &url[scheme_ind + 3..scheme_ind + 3 + end],
-        None => &url[scheme_ind + 3..],
+    let host = match url[scheme_ind..].find('/') {
+        Some(end) => &url[scheme_ind..scheme_ind + end],
+        None => &url[scheme_ind..],
     };
 
     // trim port
     let host = host.split(':').next().unwrap();
 
-    let make_ident = |url: &str| -> String {
-        let hash = hash_u64(url, registry_kind);
-        let mut raw_ident = [0u8; 16];
-        let ident = encode_hex(&hash.to_le_bytes(), &mut raw_ident);
-
-        format!("{host}-{ident}")
-    };
-
-    let (ident, url) = if registry_kind == 2 {
+    let (dir_name, url) = if kind == GIT_REPO {
         // cargo special cases github.com for reasons, so do the same
         let mut canonical = if host == "github.com" {
             url.to_lowercase()
@@ -110,8 +113,6 @@ pub fn url_to_local_dir(url: &str) -> Result<(String, String), Error> {
             canonical.truncate(query);
         }
 
-        let ident = make_ident(&canonical);
-
         if canonical.ends_with('/') {
             canonical.pop();
         }
@@ -121,19 +122,54 @@ pub fn url_to_local_dir(url: &str) -> Result<(String, String), Error> {
             canonical.truncate(canonical.len() - 4);
         }
 
-        (ident, canonical)
+        // For git repo sources, the ident is made up of the last path component
+        // which for most git hosting providers is the name of the repo itself
+        // rather than the other parts of the path that indicate user/org, but
+        // the hash is the hash of the full canonical url so still unique even
+        // for repos with the same name, but different org/user owners
+        let mut dir_name = canonical
+            .split('/')
+            .rev()
+            .next()
+            .unwrap_or("_empty")
+            .to_owned();
+
+        let hash = {
+            let mut hasher = SipHasher::new_with_keys(0, 0);
+            canonical.hash(&mut hasher);
+            hasher.finish()
+        };
+        let mut raw_ident = [0u8; 16];
+        let ident = encode_hex(&hash.to_le_bytes(), &mut raw_ident);
+
+        dir_name.push('-');
+        dir_name.push_str(ident);
+
+        (dir_name, canonical)
     } else {
-        (make_ident(url), url.to_owned())
+        let hash = {
+            let mut hasher = SipHasher::new_with_keys(0, 0);
+            kind.hash(&mut hasher);
+            url.hash(&mut hasher);
+            hasher.finish()
+        };
+        let mut raw_ident = [0u8; 16];
+        let ident = encode_hex(&hash.to_le_bytes(), &mut raw_ident);
+
+        (format!("{host}-{ident}"), url.to_owned())
     };
 
-    Ok((ident, url))
+    Ok(UrlDir {
+        dir_name,
+        canonical: url,
+    })
 }
 
 /// Get the disk location of the specified url, as well as its canonical form
 ///
 /// If not specified, the root directory is the user's default cargo home
 pub fn get_index_details(url: &str, root: Option<PathBuf>) -> Result<(PathBuf, String), Error> {
-    let (dir_name, canonical_url) = url_to_local_dir(url)?;
+    let url_dir = url_to_local_dir(url)?;
 
     let mut path = match root {
         Some(path) => path,
@@ -142,15 +178,43 @@ pub fn get_index_details(url: &str, root: Option<PathBuf>) -> Result<(PathBuf, S
 
     path.push("registry");
     path.push("index");
-    path.push(dir_name);
+    path.push(url_dir.dir_name);
 
-    Ok((path, canonical_url))
+    Ok((path, url_dir.canonical))
 }
 
 #[cfg(test)]
 mod test {
-    use super::get_index_details;
+    use super::{get_index_details, url_to_local_dir};
     use crate::PathBuf;
+
+    #[test]
+    fn canonicalizes_git_urls() {
+        let super::UrlDir { dir_name, canonical } = url_to_local_dir("git+https://github.com/EmbarkStudios/cpal.git?rev=d59b4de#d59b4decf72a96932a1482cc27fe4c0b50c40d32").unwrap();
+
+        assert_eq!("https://github.com/embarkstudios/cpal", canonical);
+        assert_eq!("cpal-a7ffd7cabefac714", dir_name);
+
+        let super::UrlDir {
+            dir_name,
+            canonical,
+        } = url_to_local_dir("git+https://github.com/gfx-rs/genmesh?rev=71abe4d").unwrap();
+
+        assert_eq!("https://github.com/gfx-rs/genmesh", canonical);
+        assert_eq!("genmesh-401fe503e87439cc", dir_name);
+
+        // For registry urls, even if they come from github, they are _not_ canonicalized
+        // and their exact url (other than the registry+ scheme modifier) is used
+        // for the hash calculation, as technically URLs are case sensitive, but
+        // in practice doesn't matter for connection purposes
+        let super::UrlDir {
+            dir_name,
+            canonical,
+        } = url_to_local_dir("registry+https://github.com/Rust-Lang/crates.io-index").unwrap();
+
+        assert_eq!("https://github.com/Rust-Lang/crates.io-index", canonical);
+        assert_eq!("github.com-016fae53232cc64d", dir_name);
+    }
 
     #[test]
     fn matches_cargo() {
@@ -182,23 +246,6 @@ mod test {
             (
                 "registry/index/dl.cloudsmith.io-ff79e51ddd2b38fd".into(),
                 "https://dl.cloudsmith.io/aBcW1234aBcW1234/embark/rust/cargo/index.git".to_owned()
-            )
-        );
-
-        // Ensure we actually strip off the irrelevant parts of a url, note that
-        // the .git suffix is not part of the canonical url, but *is* used when hashing
-        assert_eq!(
-            get_index_details(
-                &format!(
-                    "registry+{}.git?one=1&two=2#fragment",
-                    crate::CRATES_IO_INDEX
-                ),
-                Some(PathBuf::new())
-            )
-            .unwrap(),
-            (
-                "registry/index/github.com-c786010fb7ef2e6e".into(),
-                crate::CRATES_IO_INDEX.to_owned()
             )
         );
     }
