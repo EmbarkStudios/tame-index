531071cb75904dc88451feb33f3ac5e07b95cb14
Jake Shadle <jake.shadle@embark-studios.com>
2023-08-04 13:09:12 +0200

    Add concurrent/parallel helpers
    When requesting eg. several hundred crates, it's _generally_ more
    efficient to emit many concurrent requests, particularly on low core
    count machines

--- src/index/sparse_remote.rs
+++ src/index/sparse_remote.rs
@@ -2,6 +2,7 @@ use super::SparseIndex;
 use crate::{Error, IndexKrate, KrateName};
 pub use reqwest::blocking::Client;
 pub use reqwest::Client as AsyncClient;
+use std::collections::{BTreeMap, BTreeSet};
 
 /// Allows **blocking** access to a remote HTTP sparse registry index
 pub struct RemoteSparseIndex {
@@ -60,6 +61,30 @@ impl RemoteSparseIndex {
     pub fn cached_krate(&self, name: KrateName<'_>) -> Result<Option<IndexKrate>, Error> {
         self.index.cached_krate(name)
     }
+
+    /// Helper method for downloading multiple crates in parallel
+    ///
+    /// Note that in most cases using [`AsyncRemoteSparseIndex::krates_blocking`]
+    /// will outperform this method, especially on lower core counts
+    #[inline]
+    pub fn krates(
+        &self,
+        krates: BTreeSet<String>,
+        write_cache_entries: bool,
+    ) -> BTreeMap<String, Result<Option<IndexKrate>, Error>> {
+        use rayon::prelude::*;
+        krates
+            .into_par_iter()
+            .map(|kname| {
+                let res = || {
+                    let name = kname.as_str().try_into()?;
+                    self.krate(name, write_cache_entries)
+                };
+                let res = res();
+                (kname, res)
+            })
+            .collect()
+    }
 }
 
 /// Allows **async** access to a remote HTTP sparse registry index
@@ -84,10 +109,28 @@ impl AsyncRemoteSparseIndex {
         name: KrateName<'_>,
         write_cache_entry: bool,
     ) -> Result<Option<IndexKrate>, Error> {
-        let req = self.index.make_remote_request(name)?;
-        let req = req.try_into()?;
+        let req = self.index.make_remote_request(name)?.try_into()?;
+        let res = Self::exec_request(&self.client, req).await?;
 
-        let res = self.client.execute(req).await?;
+        self.index
+            .parse_remote_response(name, res, write_cache_entry)
+    }
+
+    async fn exec_request(
+        client: &AsyncClient,
+        req: reqwest::Request,
+    ) -> Result<http::Response<Vec<u8>>, Error> {
+        // This is unfortunate, but we always make a copy in case we need to retry
+        let res = loop {
+            let reqc = req.try_clone().unwrap();
+            let res = client.execute(reqc).await;
+
+            match res {
+                Err(err) if err.is_connect() || err.is_timeout() || err.is_request() => continue,
+                Err(err) => return Err(err.into()),
+                Ok(res) => break res,
+            }
+        };
 
         let mut builder = http::Response::builder()
             .status(res.status())
@@ -99,10 +142,7 @@ impl AsyncRemoteSparseIndex {
             .extend(res.headers().iter().map(|(k, v)| (k.clone(), v.clone())));
 
         let body = res.bytes().await?;
-        let res = builder.body(body.to_vec())?;
-
-        self.index
-            .parse_remote_response(name, res, write_cache_entry)
+        Ok(builder.body(body.to_vec())?)
     }
 
     /// Attempts to read the locally cached crate information
@@ -113,6 +153,91 @@ impl AsyncRemoteSparseIndex {
     #[inline]
     pub fn cached_krate(&self, name: KrateName<'_>) -> Result<Option<IndexKrate>, Error> {
         self.index.cached_krate(name)
+    }
+
+    /// Helper method for downloading multiples crates concurrently
+    ///
+    /// This method will generally perform better than [`RemoteSparseIndex::krates`]
+    ///
+    /// One notable difference with this method is that you can specify a maximum
+    /// duration that each individual krate request can take before it is timed out.
+    /// This is because certain [errors](https://github.com/seanmonstar/reqwest/issues/1748)
+    /// can occur when making many concurrent requests, which we detect and retry
+    /// automatically, but with (by default) no upper bound in number of
+    /// retries/time.
+    ///
+    /// You can also run this entire operation with a single timeout if you wish,
+    /// via something like [`tokio::time::timeout`](https://docs.rs/tokio/latest/tokio/time/fn.timeout.html)
+    pub async fn krates(
+        &self,
+        krates: BTreeSet<String>,
+        write_cache_entries: bool,
+        individual_timeout: Option<std::time::Duration>,
+    ) -> BTreeMap<String, Result<Option<IndexKrate>, Error>> {
+        let mut tasks = tokio::task::JoinSet::new();
+
+        let mut results = BTreeMap::new();
+        for kname in krates {
+            match kname
+                .as_str()
+                .try_into()
+                .and_then(|name| Ok(self.index.make_remote_request(name)?.try_into()?))
+            {
+                Ok(req) => {
+                    let client = self.client.clone();
+                    tasks.spawn(async move {
+                        let res = if let Some(to) = individual_timeout {
+                            match tokio::time::timeout(to, Self::exec_request(&client, req)).await {
+                                Ok(res) => res,
+                                Err(_) => Err(Error::Http(crate::HttpError::Timeout)),
+                            }
+                        } else {
+                            Self::exec_request(&client, req).await
+                        };
+
+                        (kname, res)
+                    });
+                }
+                Err(err) => {
+                    results.insert(kname, Err(err));
+                }
+            }
+        }
+
+        while let Some(res) = tasks.join_next().await {
+            let Ok((kname, res)) = res else { continue; };
+
+            let res = res.and_then(|res| {
+                let name = kname
+                    .as_str()
+                    .try_into()
+                    .expect("this was already validated");
+                self.index
+                    .parse_remote_response(name, res, write_cache_entries)
+            });
+
+            results.insert(kname, res);
+        }
+
+        results
+    }
+
+    /// A non-async version of [`Self::krates`]
+    ///
+    /// Using this method requires that there is an active tokio runtime as
+    /// described [here](https://docs.rs/tokio/latest/tokio/runtime/struct.Handle.html#method.current)
+    pub fn krates_blocking(
+        &self,
+        krates: BTreeSet<String>,
+        write_cache_entries: bool,
+        individual_timeout: Option<std::time::Duration>,
+    ) -> Result<BTreeMap<String, Result<Option<IndexKrate>, Error>>, tokio::runtime::TryCurrentError>
+    {
+        let current = tokio::runtime::Handle::try_current()?;
+        Ok(current.block_on(async {
+            self.krates(krates, write_cache_entries, individual_timeout)
+                .await
+        }))
     }
 }
 
